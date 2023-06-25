9fb649af0ab18e345ba85ea76ed5d9ae23300735
Kirill Dvoryaninov <kdvoryaninov@yandex.ru>
2023-06-25 19:48:53 +0300

    CORS

--- backend/app.js
+++ backend/app.js
@@ -48,6 +48,7 @@ app.use((req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
+    res.send(console.log(origin));
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
