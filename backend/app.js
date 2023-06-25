892c3a74f2c9e8ebee1644cb24abf503d073dbe7
Kirill Dvoryaninov <kdvoryaninov@yandex.ru>
2023-06-25 19:35:56 +0300

    CORS

--- backend/app.js
+++ backend/app.js
@@ -36,22 +36,22 @@ app.use(cookieParser());
 // });
 
 // Массив доменов, с которых разрешены кросс-доменные запросы
-// const allowedCors = [
-//   'https://praktikum.tk',
-//   'http://praktikum.tk',
-//   'http://daily-pix.nomoreparties.sbs',
-//   'https://daily-pix.nomoreparties.sbs/',
-//   'localhost:3000',
-// ];
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'http://daily-pix.nomoreparties.sbs',
+  'https://daily-pix.nomoreparties.sbs/',
+  'localhost:3000',
+];
 
-// app.use((req, res, next) => {
-//   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-//   // проверяем, что источник запроса есть среди разрешённых
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//   }
-//   next();
-// });
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin, true);
+  }
+  next();
+});
 
 // app.use(requestLogger);
 app.post('/signin', validateLogin, login);
